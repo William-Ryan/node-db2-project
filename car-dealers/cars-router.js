db24a7ef7b045ce6fd851da638845a2062ac3334
William <billy.ryan.rom@gmail.com>
2020-03-18 03:12:11 -0400

    Add GET Request by id/Add POST Request for new cars

--- car-dealers/cars-router.js
+++ car-dealers/cars-router.js
@@ -14,4 +14,30 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    db('cars')
+    .where({ id: req.params.id })
+    .first()
+    .then(car => {
+        if(car){
+            res.status(200).json({ data: car })
+        } else {
+            res.status(404).json({ message: "Car not found" })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: "Error retrieving the car" })
+    })
+})
+
+router.post('/', (req, res) => {
+    db('cars').insert(req.body, 'id')
+    .then(ids => {
+        res.status(201).json({ results: ids })
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error adding this Car' })
+    })
+})
+
 module.exports = router;

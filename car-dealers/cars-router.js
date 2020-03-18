76ec3c7f35f367aed6d17ad29d2d81d8d996cbff
William <billy.ryan.rom@gmail.com>
2020-03-18 03:01:50 -0400

    Add Get Request for Cars

--- car-dealers/cars-router.js
+++ car-dealers/cars-router.js
@@ -4,4 +4,14 @@ const db = require('../data/knexConfig.js')
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db('cars')
+    .then(cars => {
+        res.status(200).json(cars)
+    })
+    .catch (() => {
+        res.status(500).json({ message: 'Failed to retrieve cars' })
+    })
+})
+
 module.exports = router;
